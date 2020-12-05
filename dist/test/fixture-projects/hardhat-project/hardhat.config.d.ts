e5ff1f8c99e7a08f357ef9c7adda79efeb51f1ad
Victor Porton <porton@narod.ru>
2020-12-05 09:31:37 +0200

    removed dist/

--- dist/test/fixture-projects/hardhat-project/hardhat.config.d.ts
+++
@@ -1,5 +0,0 @@
-import '../../../src/index';
-import { HardhatUserConfig } from 'hardhat/types';
-declare const config: HardhatUserConfig;
-export default config;
-//# sourceMappingURL=hardhat.config.d.ts.map
--- dist/test/helpers.d.ts
+++
@@ -1,8 +0,0 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types';
-declare module 'mocha' {
-    interface Context {
-        env: HardhatRuntimeEnvironment;
-    }
-}
-export declare function useEnvironment(fixtureProjectName: string, networkName?: string): void;
-//# sourceMappingURL=helpers.d.ts.map
