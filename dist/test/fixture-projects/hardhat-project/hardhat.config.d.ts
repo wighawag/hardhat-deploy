88ed570cbd9f2caa7ba6a770aa10bdb4536166b3
Victor Porton <porton@narod.ru>
2020-12-05 09:27:39 +0200

    added dist/ as yarn bug workaround

---
+++ dist/test/fixture-projects/hardhat-project/hardhat.config.d.ts
@@ -0,0 +1,5 @@
+import '../../../src/index';
+import { HardhatUserConfig } from 'hardhat/types';
+declare const config: HardhatUserConfig;
+export default config;
+//# sourceMappingURL=hardhat.config.d.ts.map
