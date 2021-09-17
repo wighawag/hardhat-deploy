cf7d4f710bda402d983b0c3c8c4009737b1704db
Richard Meissner <msc.meissner@gmail.com>
2021-09-17 14:29:13 +0200

    Remove dist

--- dist/src/etherscan.d.ts
+++
@@ -1,9 +0,0 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types';
-export declare function submitSources(hre: HardhatRuntimeEnvironment, solcInputsPath: string, config?: {
-    etherscanApiKey?: string;
-    license?: string;
-    fallbackOnSolcInput?: boolean;
-    forceLicense?: boolean;
-    sleepBetween?: boolean;
-}): Promise<void>;
-//# sourceMappingURL=etherscan.d.ts.map
