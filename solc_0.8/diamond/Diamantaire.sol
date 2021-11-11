7db93d0bd73912bd8def94d5e8cd4be178a06734
Jonathan Beckman <jonathantbeckman@gmail.com>
2021-11-11 11:05:32 -0500

    Fixed compilation errors for solidity v0.8. Updated Diamond.sol to support Diamantaire.sol

--- solc_0.8/diamond/Diamantaire.sol
+++ solc_0.8/diamond/Diamantaire.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.1;
+pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
 import "./interfaces/IDiamondCut.sol";
--- solc_0.8/diamond/Diamond.sol
+++ solc_0.8/diamond/Diamond.sol
@@ -10,22 +10,28 @@ pragma solidity ^0.8.0;
 
 import { LibDiamond } from "./libraries/LibDiamond.sol";
 import { IDiamondCut } from "./interfaces/IDiamondCut.sol";
+import { IDiamondLoupe } from "./interfaces/IDiamondLoupe.sol";
+import { IERC173 } from "./interfaces/IERC173.sol";
+import { IERC165 } from "./interfaces/IERC165.sol";
 
-contract Diamond {    
+contract Diamond {
+    // more arguments are added to this struct
+    // this avoids stack too deep errors
+    struct DiamondArgs {
+        address owner;
+    }
 
-    constructor(address _contractOwner, address _diamondCutFacet) payable {        
-        LibDiamond.setContractOwner(_contractOwner);
+    constructor(IDiamondCut.FacetCut[] memory _diamondCut, DiamondArgs memory _args) payable {
+        LibDiamond.diamondCut(_diamondCut, address(0), new bytes(0));
+        LibDiamond.setContractOwner(_args.owner);
 
-        // Add the diamondCut external function from the diamondCutFacet
-        IDiamondCut.FacetCut[] memory cut = new IDiamondCut.FacetCut[](1);
-        bytes4[] memory functionSelectors = new bytes4[](1);
-        functionSelectors[0] = IDiamondCut.diamondCut.selector;
-        cut[0] = IDiamondCut.FacetCut({
-            facetAddress: _diamondCutFacet, 
-            action: IDiamondCut.FacetCutAction.Add, 
-            functionSelectors: functionSelectors
-        });
-        LibDiamond.diamondCut(cut, address(0), "");        
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
+
+        // adding ERC165 data
+        ds.supportedInterfaces[type(IERC165).interfaceId] = true;
+        ds.supportedInterfaces[type(IDiamondCut).interfaceId] = true;
+        ds.supportedInterfaces[type(IDiamondLoupe).interfaceId] = true;
+        ds.supportedInterfaces[type(IERC173).interfaceId] = true;
     }
 
     // Find facet for function that is called and execute the
