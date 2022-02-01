f44ec2a71e23e3b499dde03bc7dc70eff9745424
wighawag <wighawag@gmail.com>
2022-02-01 17:31:59 +0000

    fix

--- src/helpers.ts
+++ src/helpers.ts
@@ -1910,7 +1910,7 @@ Note that in this case, the contract deployment will not behave the same if depl
       const selectorsToReplace: string[] = [];
 
       for (const selector of newFacet.functionSelectors) {
-        if (oldSelectors.indexOf(selector) > 0) {
+        if (oldSelectors.indexOf(selector) >= 0) {
           if (
             oldSelectorsFacetAddress[selector].toLowerCase() !==
             newFacet.facetAddress.toLowerCase()
