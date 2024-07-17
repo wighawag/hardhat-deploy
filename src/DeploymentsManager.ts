62db6ca1e3ed7ffbe0a57e9fe16f7eda9d1326d4
Gonzalo Othacehe <gonza.otc@gmail.com>
2024-07-17 11:55:51 -0300

    Fixed DeploymentsManager function name typo

--- src/DeploymentsManager.ts
+++ src/DeploymentsManager.ts
@@ -581,7 +581,7 @@ export class DeploymentsManager {
     return this.db.unnamedAccounts;
   }
 
-  private async getDeterminisityDeploymentInfo(): Promise<
+  private async getDeterministicDeploymentInfo(): Promise<
     DeterministicDeploymentInfo | undefined
   > {
     const chainId = await this.getChainId();
@@ -590,22 +590,22 @@ export class DeploymentsManager {
   }
 
   public async getDeterministicDeploymentFactoryAddress(): Promise<string> {
-    const info = await this.getDeterminisityDeploymentInfo();
+    const info = await this.getDeterministicDeploymentInfo();
     return info?.factory || '0x4e59b44847b379578588920ca78fbf26c0b4956c';
   }
 
   public async getDeterministicDeploymentFactoryDeployer(): Promise<string> {
-    const info = await this.getDeterminisityDeploymentInfo();
+    const info = await this.getDeterministicDeploymentInfo();
     return info?.deployer || '0x3fab184622dc19b6109349b94811493bf2a45362';
   }
 
   public async getDeterministicDeploymentFactoryFunding(): Promise<BigNumber> {
-    const info = await this.getDeterminisityDeploymentInfo();
+    const info = await this.getDeterministicDeploymentInfo();
     return BigNumber.from(info?.funding || '10000000000000000');
   }
 
   public async getDeterministicDeploymentFactoryDeploymentTx(): Promise<string> {
-    const info = await this.getDeterminisityDeploymentInfo();
+    const info = await this.getDeterministicDeploymentInfo();
     return (
       info?.signedTx ||
       '0xf8a58085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf31ba02222222222222222222222222222222222222222222222222222222222222222a02222222222222222222222222222222222222222222222222222222222222222'
