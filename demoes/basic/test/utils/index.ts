ba36c0202d987836e14960ddea314d55023019d5
wighawag <wighawag@gmail.com>
2025-03-12 21:05:20 +0000

    update README

--- demoes/basic/test/utils/index.ts
+++ demoes/basic/test/utils/index.ts
@@ -14,7 +14,7 @@ export function setupFixtures(provider: EthereumProvider) {
 			);
 
 			// Deployment are inherently untyped since they can vary from network or even before different from current artifacts
-			// so here we type them manually
+			// so here we type them manually assuming the artifact is still matching
 			const GreetingsRegistry = env.get<typeof env.artifacts.GreetingsRegistry.abi>('GreetingsRegistry');
 
 			return {env, GreetingsRegistry, namedAccounts: env.namedAccounts, unnamedAccounts: env.unnamedAccounts};
