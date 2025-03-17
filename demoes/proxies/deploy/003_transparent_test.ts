a00f4dd2b408a61fa226b972f10c5986b4e82e79
wighawag <wighawag@gmail.com>
2025-03-17 19:09:11 +0000

    add transparent example

---
+++ demoes/proxies/deploy/003_transparent_test.ts
@@ -0,0 +1,43 @@
+import {execute, artifacts} from '@rocketh';
+
+export default execute(
+	async ({deployViaProxy, namedAccounts}) => {
+		const {deployer, admin} = namedAccounts;
+
+		const prefix = 'proxy:';
+		await deployViaProxy(
+			'Transparent',
+			{
+				account: deployer,
+				artifact: artifacts.GreetingsRegistry,
+				args: [prefix],
+			},
+			{
+				owner: admin,
+				proxyContract: 'SharedAdminOptimizedTransparentProxy',
+				linkedData: {
+					prefix,
+					admin,
+				},
+			},
+		);
+
+		await deployViaProxy(
+			'Transparent',
+			{
+				account: deployer,
+				artifact: artifacts.GreetingsRegistry2,
+				args: [prefix],
+			},
+			{
+				owner: admin,
+				proxyContract: 'SharedAdminOptimizedTransparentProxy',
+				linkedData: {
+					prefix,
+					admin,
+				},
+			},
+		);
+	},
+	{tags: ['Transparent', 'Transparent_deploy']},
+);
