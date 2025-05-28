4806ad5277a505cb46ed655c6f83d9e81e803117
wighawag <wighawag@gmail.com>
2025-05-28 15:56:00 +0100

    upgrade to latest hardhat + fix

--- packages/hardhat-deploy/src/config/validation.ts
+++ packages/hardhat-deploy/src/config/validation.ts
@@ -4,19 +4,21 @@ import type {HardhatUserConfig} from 'hardhat/config';
 import {validateUserConfigZodType} from '@nomicfoundation/hardhat-zod-utils';
 import {z} from 'zod';
 
-const artifactGenerationUserConfigSchema = z.object({
-	// externalArtifacts: z.array(z.string()).optional(),
-	destinations: z
-		.object({
-			js: z.array(z.string()).optional(),
-			ts: z.array(z.string()).optional(),
-			json: z.array(z.string()).optional(),
-			jsm: z.array(z.string()).optional(),
-			tsm: z.array(z.string()).optional(),
-			directories: z.array(z.string()).optional(),
-		})
-		.optional(),
-});
+const artifactGenerationUserConfigSchema = z
+	.object({
+		// externalArtifacts: z.array(z.string()).optional(),
+		destinations: z
+			.object({
+				js: z.array(z.string()).optional(),
+				ts: z.array(z.string()).optional(),
+				json: z.array(z.string()).optional(),
+				jsm: z.array(z.string()).optional(),
+				tsm: z.array(z.string()).optional(),
+				directories: z.array(z.string()).optional(),
+			})
+			.optional(),
+	})
+	.optional();
 
 export async function validateTypechainUserConfig(
 	userConfig: HardhatUserConfig
